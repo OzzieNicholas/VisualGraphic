f546306e3af38bc1b8850a1a828a1d30592ca8a4
LuoYi <2998622373@qq.com>
2025-05-08 11:42:20 +0800

    feat: 添加三维坐标轴

--- CGVisual/BasicAxis.h
+++ CGVisual/BasicAxis.h
@@ -1,16 +1,45 @@
 #pragma once
 
-#include <QtWidgets/QMainWindow>
-#include "ui_BasicAxis.h"
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLBuffer>
 
-class BasicAxis : public QMainWindow
+class BasicAxis : public QOpenGLWidget, protected QOpenGLFunctions
 {
     Q_OBJECT
 
 public:
-    BasicAxis(QWidget *parent = nullptr);
+    BasicAxis(QWidget* parent = nullptr);
     ~BasicAxis();
 
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+
 private:
-    Ui::BasicAxisClass ui;
+    QOpenGLShaderProgram* shaderProgram;
+    QOpenGLVertexArrayObject vao;
+    QOpenGLBuffer vbo;
+
+    const char* vertexShader = R"(
+        #version 460 core
+        layout(location=0) in vec3 position;
+        layout(location=1) in vec3 color;
+        uniform mat4 mvp;
+        out vec3 vColor;
+        void main(){
+            gl_Position = mvp * vec4(position, 1.0);
+            vColor = color;
+        })";
+
+    const char* fragmentShader = R"(
+        #version 460 core
+        in vec3 vColor;
+        out vec4 fragColor;
+        void main(){
+            fragColor = vec4(vColor, 1.0);
+        })";
 };
